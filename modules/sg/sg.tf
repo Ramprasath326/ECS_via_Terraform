c0984b3568e2b3cf6ad283b841b4f148bb2e627f
ramprasath326 <ramprasathspare@gmail.com>
2026-06-23 14:02:45 +0530

    after enabling outbound traffic in api sg

--- modules/sg/sg.tf
+++ modules/sg/sg.tf
@@ -26,7 +26,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_https" {
 
 
 #Allow all outbound traffic
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_web_outbound" {
   security_group_id = aws_security_group.web_sg.id
   cidr_ipv4         = var.cidr_block
   ip_protocol       = "-1" # semantically equivalent to all ports
@@ -52,4 +52,10 @@ resource "aws_vpc_security_group_ingress_rule" "allow_from_web_sg" {
   from_port         = 80
   ip_protocol       = "tcp"
   to_port           = 80
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_api_outbound" {
+  security_group_id = aws_security_group.api_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
 }
