016dc336e451c0b6ace88c1b4166d7e4f1b660b6
ramprasath326 <ramprasathspare@gmail.com>
2026-07-10 08:42:27 +0530

    ec2-imported

---
+++ import.tf
@@ -0,0 +1,10 @@
+#resource "aws_instance" "Unix-vm-imported" {
+#  instance_type = "t3.micro"
+#  ami           = "ami-0b6d9d3d33ba97d99"
+#}
+
+import{
+    to = aws_instance.Unix-vm-imported
+    id = "i-041d3e8e5d302f2ff"
+
+}
